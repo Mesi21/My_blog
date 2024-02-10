bd7acb6f5ca78d1f58da3b9c090acf77b0d3d274
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-02-10 20:33:07 +0200

    Add statements to make controllers render json request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    json_response(@users)
   end
 
   def show
     @user = User.find(params[:id])
+    json_response(set_user)
   end
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
